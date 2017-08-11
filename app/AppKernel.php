e02d631ad5de1b322c0c24941b045c4d2b2c2a3a
Clement Yann <clementyann@free.fr>
2017-08-11 07:53:57 +0200

    mise à jour

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Louvre\FrontBundle\LouvreFrontBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Endroid\QrCode\Bundle\EndroidQrCodeBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
